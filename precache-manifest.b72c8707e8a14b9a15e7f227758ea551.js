8fae80a8dc84c4fadd7c4ac2e89d4de80686b97a
nikabutikashvili <65020676+nikabutikashvili@users.noreply.github.com>
2020-09-06 11:26:37 +0400

    Updates

---
+++ precache-manifest.b72c8707e8a14b9a15e7f227758ea551.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ef8fe5d3ddc195840b0097a85b2ead",
+    "url": "/Drop-Co/index.html"
+  },
+  {
+    "revision": "4772909b2af69ddb33ac",
+    "url": "/Drop-Co/static/css/main.363f1f0c.chunk.css"
+  },
+  {
+    "revision": "a9c8516115395f20bb37",
+    "url": "/Drop-Co/static/js/2.8c8b356c.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Drop-Co/static/js/2.8c8b356c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4772909b2af69ddb33ac",
+    "url": "/Drop-Co/static/js/main.02757bd9.chunk.js"
+  },
+  {
+    "revision": "5e694a52b1ae98064ef4",
+    "url": "/Drop-Co/static/js/runtime-main.6a5f3d96.js"
+  },
+  {
+    "revision": "92ce138dbbe68de87c10374992dc5014",
+    "url": "/Drop-Co/static/media/barred.92ce138d.jpg"
+  },
+  {
+    "revision": "bb3b7d4e10ec87ca041e894975fbcf0d",
+    "url": "/Drop-Co/static/media/coffeegreen.bb3b7d4e.jpg"
+  },
+  {
+    "revision": "fd90ed7197d83e397cb1d35bf8b0c31f",
+    "url": "/Drop-Co/static/media/coffeeseeds.fd90ed71.jpeg"
+  },
+  {
+    "revision": "d1dcd8ef0c94b1f73bc4b88f2a06899e",
+    "url": "/Drop-Co/static/media/coffeeshop.d1dcd8ef.jpeg"
+  },
+  {
+    "revision": "298f1f4437991737880fe628b7da1202",
+    "url": "/Drop-Co/static/media/instagram.298f1f44.png"
+  },
+  {
+    "revision": "d9953c681825bb77c7d6d891fe533bcd",
+    "url": "/Drop-Co/static/media/kitchen.d9953c68.jpeg"
+  },
+  {
+    "revision": "e5305f5fa9eeb8acf3ff8498921060a1",
+    "url": "/Drop-Co/static/media/logo.e5305f5f.png"
+  },
+  {
+    "revision": "c0cfc94c0c0683a37867dcea731f80ac",
+    "url": "/Drop-Co/static/media/minus.c0cfc94c.png"
+  },
+  {
+    "revision": "7e6eff77e863f0e839e1889734b9696d",
+    "url": "/Drop-Co/static/media/plus.7e6eff77.png"
+  },
+  {
+    "revision": "bdb761af652dc71ff4724b23e9d14068",
+    "url": "/Drop-Co/static/media/snacks.bdb761af.jpeg"
+  },
+  {
+    "revision": "e696e577fd50508e08ec5144035747f6",
+    "url": "/Drop-Co/static/media/snapchat.e696e577.png"
+  },
+  {
+    "revision": "aaf6069d2f7ed7f16af34490e6845811",
+    "url": "/Drop-Co/static/media/teaseeds.aaf6069d.jpg"
+  },
+  {
+    "revision": "c228a51f52d262f60b78c320bb8fb782",
+    "url": "/Drop-Co/static/media/twitter.c228a51f.png"
+  }
+]);
